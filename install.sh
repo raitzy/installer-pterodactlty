0ffaeb5fdf93c67cc94ec28306d8e933637dd23c
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-10-19 18:33:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,105 +20,7 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 # Display welcome message
-display_welcome() {
-  echo -e ""
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]                                                 [+]${NC}"
-  echo -e "${BLUE}[+]             AUTO INSTALLER PTERODACTYL          [+]${NC}"
-  echo -e "${BLUE}[+]                  © BY WANNFYY                   [+]${NC}"
-  echo -e "${BLUE}[+]                                                 [+]${NC}"
-  echo -e "${RED}[+] ===============================================  [+]${NC}"
-  echo -e ""
-  echo -e "script ini di buat untuk mempermudah penginstalasian pterodactyle,"
-  echo -e "dilarang keras untuk memperjual belikan."
-  echo -e ""
-  echo -e "JANGAN LUPA SUBSCRIBE @WANNOFFC"
-  echo -e ""
-  echo -e "TQ TO :"
-  echo -e "ALL CREATOR WHATSAPP"
-  echo -e "WANNOFFICIAL ( MY SELF )"
-  sleep 4
-  clear
-}
 
-#Update and install jq
-install_jq() {
-  echo -e "                                                       "
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]             UPDATE & INSTALL JQ                 [+]${NC}"
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "                                                       "
-  sudo apt update && sudo apt install -y jq
-  if [ $? -eq 0 ]; then
-    echo -e "                                                       "
-    echo -e "${GREEN}[+] =============================================== [+]${NC}"
-    echo -e "${GREEN}[+]              INSTALL JQ BERHASIL                [+]${NC}"
-    echo -e "${GREEN}[+] =============================================== [+]${NC}"
-  else
-    echo -e "                                                       "
-    echo -e "${RED}[+] =============================================== [+]${NC}"
-    echo -e "${RED}[+]              INSTALL JQ GAGAL                   [+]${NC}"
-    echo -e "${RED}[+] =============================================== [+]${NC}"
-    exit 1
-  fi
-  echo -e "                                                       "
-  sleep 1
-  clear
-}
-#Check user token
-check_token() {
-  echo -e "                                                       "
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY VALLZ OFFC             [+]${NC}"
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "                                                       "
-  echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
-  read -r USER_TOKEN
-
-  if [ "$USER_TOKEN" = "iniwannbroku" ]; then
-    echo -e "${GREEN}AKSES BERHASIL${NC}}"
-  else
-    echo -e "${GREEN} Silahkan Tanyakan Kepada WannOFFICIAL Jika Token Salah.${NC}"
-    exit 1
-  fi
-  clear
-}
-
-create_node() {
-  echo -e "                                                       "
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]                    CRRATE NODE                 [+]${NC}"
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "                                                       "
-  #!/bin/bash
-#!/bin/bash
-
-# Minta input dari pengguna
-read -p "Masukkan nama lokasi: " location_name
-read -p "Masukkan deskripsi lokasi: " location_description
-read -p "Masukkan domain: " domain
-read -p "Masukkan nama node: " node_name
-read -p "Masukkan RAM (dalam MB): " ram
-read -p "Masukkan jumlah maksimum disk space (dalam MB): " disk_space
-read -p "Masukkan Locid: " locid
-
-# Ubah ke direktori pterodactyl
-cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
-
-# Membuat lokasi baru
-php artisan p:location:make <<EOF
-$location_name
-$location_description
-EOF
-
-# Membuat node baru
-php artisan p:node:make <<EOF
-$node_name
-$location_description
-$locid
-https
-$domain
-yes
 no
 no
 $ram
@@ -212,7 +114,7 @@ while true; do
   clear
 
   case "$MENU_CHOICE" in
-      3)
+      
       configure_wings
       ;;
       4)
